4a6d95d363402c66a87138c8a9deb94a69fd554f
“sawa” <silkyworkspace@gmail.com>
2025-11-10 22:41:08 +0900

    REAME更新とvite.configにエイリアス追記

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+// __dirname の代用（ESM対応）
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: "/bamosDesign_blogSite/",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
